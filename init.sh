a8474a514d514f6e92b304db8e9c1797c5ed489e
lyyyuna <lyyyuna@outlook.com>
2020-11-30 19:22:33 +0800

    update

--- init.sh
+++ init.sh
@@ -3,25 +3,27 @@
 WORKSPACE=`pwd`
 cd $WORKSPACE/3rd_party
 
+VERSION=4.1.0
+
 # libosip2
-tar xvf libosip2-4.1.0.tar.gz
-cd libosip2-4.1.0
+tar xvf libosip2-$VERSION.tar.gz
+cd libosip2-$VERSION
 
 ./configure --prefix=$WORKSPACE
 make install
 
 cd $WORKSPACE/3rd_party
-rm -rf libosip2-4.1.0
+rm -rf libosip2-$VERSION
 
 # libexosip2
-tar xvf libexosip2-4.1.0.tar.gz
-cd libexosip2-4.1.0
+tar xvf libeXosip2-$VERSION.tar.gz
+cd libeXosip2-$VERSION
 
 ./configure --prefix=$WORKSPACE
 make install
 
 cd $WORKSPACE/3rd_party
-rm -rf libexosip2-4.1.0
+rm -rf libeXosip2-$VERSION
 
 # rm unused files
 cd $WORKSPACE
@@ -29,5 +31,5 @@ cd $WORKSPACE
 rm -rf share
 rm -rf lib/pkgconfig
 rm lib/*.dylib || true
-rm lib/*.so || true
+rm lib/*.so* || true
 rm lib/*.la || true
